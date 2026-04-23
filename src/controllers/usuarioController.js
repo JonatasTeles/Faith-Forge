30d6c672f6014965a9716a905c59a17c96180d3f
JonatasTeles <jonatas.teles@sptech.school>
2026-04-23 13:53:53 -0300

    Arrumando o back do login e cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -17,8 +17,8 @@ function autenticar(req, res) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
+                    // if (resultadoAutenticar.length == 1) {
+                        // console.log(resultadoAutenticar);
                         /* aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
                             .then((resultadoAquarios) => {
                                 if (resultadoAquarios.length > 0) {
@@ -33,6 +33,15 @@ function autenticar(req, res) {
                                     res.status(204).json({ aquarios: [] });
                                 }
                             }) */
+
+                        if (resultadoAutenticar.length == 1) {
+                            res.json({
+                                id: resultadoAutenticar[0].id,
+                                email: resultadoAutenticar[0].email,
+                                nome: resultadoAutenticar[0].nome,
+                                senha: resultadoAutenticar[0].senha
+                            })
+
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -51,7 +60,6 @@ function autenticar(req, res) {
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
